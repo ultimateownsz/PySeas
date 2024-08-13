962effce4120f291dd31e28a7690c69d62d80087
marcel <marcel.phippen@proton.me>
2024-08-13 14:31:58 +0200

    lint py_vertion/player.py

--- src/py_version/player.py
+++ src/py_version/player.py
@@ -1,3 +1,4 @@
+""" player object that hold stats and RNG """
 from random import randint
 from dataclasses import dataclass, field
 from typing import List
@@ -8,7 +9,7 @@ from .inventory import Inventory
 
 @dataclass
 class Player:
-
+    """ hold the imventory, money, posision and name of a player """
     name_of_player: str
     player_id: int
     inventory: List[str] = field(default_factory=list)
@@ -23,18 +24,29 @@ class Player:
     # self.roll_history_p2 = []
 
     def print_info(self):
+        """ print stats of a player """
         print(
-            f"Arrr.. Mateyy {self.name_of_player.capitalize()} Down below you can see your stats:\n"
+            f"Arrr.. Mateyy {self.name_of_player.capitalize()} Down below you can see your stats:"
         )
         print(
-            f"Player: {self.name_of_player.capitalize()}\nInventory: {self.inventory}\nWallet: {self.wallet}\nBoard index: {self.board_index}\n"
+            f"""
+            Player: {self.name_of_player.capitalize()}
+            Inventory: {self.inventory}
+            Wallet: {self.wallet}
+            Board index: {self.board_index}
+            """
         )
 
     def dice_roll(self):
+        """ get random numbers """
         roll_results = [randint(1, 6) for _ in range(2)]
         total_roll = sum(roll_results)
         print(
-            f"\n{self.name_of_player.capitalize()} throws the dices {roll_results[0]} and {roll_results[1]}. You rolled a total of {sum(roll_results)}\n"
+            '\n',
+            self.name_of_player.capitalize(),
+            ' throws the dices ', roll_results[0], ' and ', roll_results[1],
+            '. You rolled a total of ', sum(roll_results),
+            '\n'
         )
         # if self.player_id == 1:
         #     self.roll_history_p1.append(roll_results)
@@ -52,12 +64,14 @@ class Player:
             player=self, total_roll=self.dice_roll()
         )
         print(
-            f"{self.name_of_player.capitalize()} moves to {board.locations_with_index[new_position]}\n"
+            self.name_of_player.capitalize(),
+            ' moves to ',
+            board.locations_with_index[new_position]
         )
         return new_position
 
     def perform_action(self, board, new_position):
-        """Made this function to call the board everytime the player got on a tile to call the function"""
+        """ call the board everytime the player got on a tile to call the function"""
         board.visit_locations_by_index(new_position)
         if new_position in board.locations_actions:
             board.locations_actions[new_position]()
