8613fbdd56aa1753f0b6a38b868a128a14bcb420
marcel <marcel.phippen@proton.me>
2024-08-15 21:03:38 +0200

    ignore the pytmx untyped module

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from typing import List
 
 # import pygame related
 import pygame
-from pytmx.util_pygame import load_pygame
+from pytmx.util_pygame import load_pygame  # type: ignore
 
 # import Python specific objects, functions and functionality
 from src.CLI.board import Board
