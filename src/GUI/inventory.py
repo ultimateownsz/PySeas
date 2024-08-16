0d7b49e3cdd65bde4b4d97fa647a1dde6c8a8028
ultimateownsz <saiutechnologies@outlook.com>
2024-08-17 01:01:18 +0200

    added typechecks from a prior PR
    - board.py has now all the right typechecks, if we do decide to remake the CLI game someday we have a good start to begin with
    - inventory is now with this commit, I forgot it with the other commit

--- src/GUI/inventory.py
+++ src/GUI/inventory.py
@@ -5,19 +5,22 @@ this file contain types of items, like Chest
 
 
 class Chest:
-    """ contain loot, and worth """
+    """contain loot, and worth"""
+
     def __init__(self, name: str) -> None:
         self.name = name
 
 
 class Quest:
-    """ TODO: make the docstrings """
+    """TODO: make the docstrings"""
+
     def __init__(self) -> None:
         self.completed = False
 
 
 class Inventory:
-    """ contain money, chests, quests """
+    """contain money, chests, quests"""
+
     def __init__(self) -> None:
         # special attribute
         self.money: int = 0
@@ -27,17 +30,17 @@ class Inventory:
         self.quests: list[Quest] = []
 
     def add_chest(self, chest: Chest) -> None:
-        """ apend a chest to the inventory """
+        """append a chest to the inventory"""
         self.chests.append(chest)
 
     def get_chests(self) -> list[Chest]:
-        """ Return a copy of the chests list """
+        """Return a copy of the chests list"""
         return self.chests.copy()
 
     def add_quest(self, quest: Quest) -> None:
-        """ apend a quest to the inventory """
+        """append a quest to the inventory"""
         self.quests.append(quest)
 
     def get_quests(self) -> list[Quest]:
-        """ Return a copy of the quests list """
+        """Return a copy of the quests list"""
         return self.quests.copy()
